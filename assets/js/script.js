e241f4011c3be64df1ee0df0bf20d491f307ad0b
Jarmaris Chaney <jarmaris.chaney@yahoo.com>
2022-06-11 10:24:46 -0500

    created html file

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// Create landing page 
+// tell user info about quiz
+// h1 or p
+// start button to initiate quiz
+// call a function
+// taking user to quiz
+// hiding the landing page
+// going to new html
+// display those quize q's
+// display first question on new HTML
+// alter HTML with JS!!
+// STRETCH
+// pass criteria
+
+// User clicks start
+// heading for quiz name
+// plain HTML
+// score
+// function for points on a question
+// 
+// time (check readme)
+// function
+// first quiz question
+// 4 possible answers
+// once button is clicked show next question
+// next question function
+// alter the score
+// deduct time
+
+
+// User answers all Q
+// show final score
+// function show final score
+// save score
+// function local storage
+// list of high scores
+// STRETCH
+// passed or failed
+// array of objects
+var charley = 0
+console.log(charley)
+
+function tester() {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+charley++
+console.log(charley)
+}
+
+tester()
